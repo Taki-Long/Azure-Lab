6fa68d7079fe8f05a410c5c3dbfcc26b629ab5e1
taki <nbjwl@live.com>
2023-09-04 00:57:09 +0800

    add pipeline

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ data "azurerm_resource_group" "azure_lab" {
 
 resource "azurerm_static_site" "azure_lab" {
   name                = "azure_lab"
-  location            = azurerm_resource_group.azure_lab.location
-  resource_group_name = azurerm_resource_group.azure_lab.name
+  location            = data.azurerm_resource_group.azure_lab.location
+  resource_group_name = data.azurerm_resource_group.azure_lab.name
   sku_tier = "Free"
   sku_size = "Free"
 }
