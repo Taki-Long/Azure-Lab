bf74d07cff24294cf4a4f77bdbcf7c39dd8e86e9
taki <nbjwl@live.com>
2023-09-10 02:08:55 +0800

    terraform

--- main.tf
+++ main.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">3.0.0"
+      version = "=3.71.0"
     }
   }
 }
 
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  storage_use_azuread = true
   features {
     key_vault {
       purge_soft_delete_on_destroy    = true
@@ -17,30 +18,31 @@ provider "azurerm" {
   }
 }
 
-data "azurerm_resource_group" "lab" {
-  name = "Azure-Lab"
+resource "azurerm_resource_group" "lab" {
+  name = "azure-lab-rg"
+  location = "Central US"
 }
 
 resource "azurerm_storage_account" "lab" {
-  name                     = "azurelabinfra"
-  location                 = data.azurerm_resource_group.lab.location
-  resource_group_name      = data.azurerm_resource_group.lab.name
+  name                     = "azurelabsa"
+  location                 = azurerm_resource_group.lab.location
+  resource_group_name      = azurerm_resource_group.lab.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "lab" {
-  name                = "azure-lab"
-  location            = data.azurerm_resource_group.lab.location
-  resource_group_name = data.azurerm_resource_group.lab.name
+  name                = "azure-lab-asp"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
   os_type             = "Linux"
   sku_name            = "Y1"
 }
 
 resource "azurerm_linux_function_app" "lab" {
   name                = "azure-lab-func"
-  location            = data.azurerm_resource_group.lab.location
-  resource_group_name = data.azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
 
   storage_account_name       = azurerm_storage_account.lab.name
   storage_account_access_key = azurerm_storage_account.lab.primary_access_key
@@ -59,8 +61,8 @@ resource "azurerm_linux_function_app" "lab" {
 
 resource "azurerm_static_site" "lab" {
   name                = "azure-lab-web"
-  location            = data.azurerm_resource_group.azure_lab.location
-  resource_group_name = data.azurerm_resource_group.azure_lab.name
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
   sku_tier            = "Free"
   sku_size            = "Free"
 }
