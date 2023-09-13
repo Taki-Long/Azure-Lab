b77f209bb55c886a5ea3fad025f099ba5864179c
taki <nbjwl@live.com>
2023-09-13 17:28:30 +0800

    update

--- src/web/app/login/page.tsx
+++ src/web/app/login/page.tsx
@@ -1,5 +1,3 @@
-import Chatbot from "@/components/ChatWindow";
-
 export default function Home() {
   return (
     <a href="/.auth/login/github">Login with Github</a>
--- src/web/components/ChatWindow.tsx
+++ src/web/components/ChatWindow.tsx
@@ -1,29 +1,47 @@
 'use client'
-
+import { useState } from 'react';
 import { Card, TextArea } from '@radix-ui/themes';
 import { Button} from '@radix-ui/themes';
 import useSWR from 'swr'
+import { json } from 'stream/consumers';
 
 const fetcher = (url: RequestInfo | URL) => fetch(url).then(r => r.json())
 
+
 const Chatbot = () => {
   const { data, error, isLoading } = useSWR('/.auth/me', fetcher)
-  let hello
+  let userinfo
   if (isLoading) {
-    hello = <div>loading...</div>
+    userinfo = <div>loading...</div>
   } else {
-    hello = <div>Hello {data.clientPrincipal.userDetails}</div>
+    userinfo = <div>User: {data.clientPrincipal.userDetails}</div>
   }
+
+  const [hello, setHello] = useState("");
+  const [input, setInput] = useState("hello");
+
+  const handleClick = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    fetch("/api/hello",{
+      method: "POST",
+      body: JSON.stringify({
+        "name": input
+      })
+    }).then((r)=>r.text()).then((d)=>setHello(d))
+  };
+  
   return (
     <div className="flex flex-col h-screen space-y-4 p-8">
       <Card className="grow">
-        {hello}
+        {userinfo}
+        CallApi: {hello}
       </Card>
       <div className="flex flex-row items-center space-x-4">
         <div className="grow content-center">
-          <TextArea></TextArea>
+          <TextArea value={input} onChange={e=>setInput(e.target.value)}></TextArea>
         </div>
-        <Button size="4" className="w-32">Send</Button>
+        <Button onClick={handleClick} size="4" className="w-32">Send</Button>
       </div>
     </div>
   );
